404d450a9fc5d9f4fa39bd51e8fea9eb9c716d05
HafenaajeA <almandocode@gmail.com>
2024-06-01 11:01:49 +0200

    loading bar

---
+++ app.js
@@ -0,0 +1,17 @@
+const counterEl = document.querySelector(".counter");
+const barEl = document.querySelector(".loading-bar-front");
+
+let idx = 0;
+
+updateNum();
+
+function updateNum() {
+  // It waas possibie to write it 20% but we have to write it dynamic
+  counterEl.innerText = idx + "%";
+  barEl.style.width = idx + "%";
+  idx++;
+  if (idx < 105) {
+    // this has to load every 20ms
+    setTimeout(updateNum, 20);
+  }
+}
